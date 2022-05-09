4cdacb9c1f732e27bea2da6662793b896406c4e3
Maskott <themaskott@gmail.com>
2022-05-09 15:23:04 +0200

    check if prefix already annouced

--- get_IP.py
+++ get_IP.py
@@ -10,17 +10,17 @@ def get_annouced_IP( AS_file:str, source:str ):
     AS = load_json_file(AS_file)
 
     for l in open(source, "r"):
-        annoucer = l.split('|')[6].split(' ')[-1]
-        annoucer = "AS" + annoucer
+        announcer = l.split('|')[6].split(' ')[-1]
+        announcer = "AS" + announcer
         p = l.split('|')[5]
         ip = p.split('/')[0]
         if IPAddress(ip).version == 4:
-            annouced = p
-            if annoucer in AS:
-                if "annouced_IP" in AS[annoucer]:
-                    AS[annoucer]["annouced_IP"].append(annouced)
+            announced = p
+            if announcer in AS:
+                if "announced_IP" in AS[announcer] and announced not in AS[announcer]["announced_IP"]:
+                    AS[announcer]["announced_IP"].append(announced)
                 else:
-                    AS[annoucer].update({"annouced_IP":[annouced]})
+                    AS[announcer].update({"announced_IP":[announced]})
 
-        with open("AS.json", "w") as out:
+        with open("datas/AS.json", "w") as out:
             json.dump(AS, out)
